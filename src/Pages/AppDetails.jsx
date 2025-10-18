e065ab1283d91bcfbad03830baf1bcf0ee51e28d
Chayan Bala <chayankumarb1992@gmail.com>
2025-10-18 22:42:07 +0600

    Update

--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -74,7 +74,6 @@ const AppDetailsCard = () => {
             <div className="w-full md:w-1/3 flex items-center justify-center p-8 bg-white">
               <div className="relative w-40 h-40 sm:w-56 sm:h-56 rounded-full bg-blue-50 flex items-center justify-center">
                 <img src={image} alt="App image" />
-                <div className="absolute w-20 h-20 border-4 border-blue-600 rounded-lg opacity-40"></div>
               </div>
             </div>
 
